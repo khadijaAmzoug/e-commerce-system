6f64f74575c6b722a902cd7e6226c2d9d7239013
khadijaamzoug <khadijasrea@gmail.com>
2025-06-06 21:37:32 +0200

    add all files

--- dist/utils/discountCalculator.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.calculateDiscount = calculateDiscount;
-function calculateDiscount(price, discountPercentage) {
-    const discountAmount = (price * discountPercentage) / 100;
-    return parseFloat(discountAmount.toFixed(2));
-}
--- dist/utils/taxCalculator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.calculateTax = calculateTax;
-function calculateTax(price, category) {
-    const taxRate = category.toLowerCase() === 'groceries' ? 0.03 : 0.0475;
-    const taxAmount = price * taxRate;
-    return parseFloat(taxAmount.toFixed(2));
-}
